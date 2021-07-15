1c6cb0d98a37484057ab86ce4d3f5987b6051572
tetsu.hira <svc.snowdrop@gmail.com>
2021-07-15 09:15:29 +0900

    7/15朝の状態

--- app.js
+++ app.js
@@ -43,8 +43,6 @@ app.get('/index', (req, res) => {
   connection.query(
     'SELECT * FROM team; SELECT * FROM court',
     (error, results) => {
-      console.log(results[0]);
-      console.log(results[1]);
       res.render('index.ejs', {items: results[0], courts: results[1]});
     }
   );
@@ -53,11 +51,9 @@ app.get('/index', (req, res) => {
 // DBから表に反映させるルーティング
 app.get('/match', (req, res) => {
   connection.query(
-    'SELECT * FROM team',
+    'SELECT * FROM team; SELECT * FROM court; SELECT * FROM game',
     (error, results) => {
-      console.log("確認につかう");
-      console.log(results);
-      res.render('match.ejs', {teams: results});
+      res.render('match.ejs', {teams: results[0], courts: results[1], games: results[2]});
     }
   );
 });
@@ -68,23 +64,68 @@ app.get('/edit/:id', (req, res) => {
     'SELECT * FROM team WHERE id = ?; SELECT * FROM court',
     [ req.params.id ],
     (error, results) => {
-      console.log("配列修正の結果↓");
-      console.log(results[0]);
-      console.log(results[1]);
       res.render('edit.ejs', {teams: results[0], courts: results[1]});
     }
   );
 });
 // DBを更新するルーティング
 app.post('/update/:id', (req, res) => {
-  console.log("ここから結果");
-  console.log(req.params.id);
-  console.log("ここまで")
   connection.query(
-    'UPDATE team SET court = ? WHERE id = ?',
-    [ req.body.itemName, req.params.id ],
+    'UPDATE team SET court = ? WHERE id = ?;UPDATE team SET number = ?  WHERE id = ?',
+    [ req.body.itemName, req.params.id, req.body.itemNum, req.params.id ],
     (error, results) => {
       res.redirect('/index');
+    }
+  );
+});
+// 点数を登録するルーティング
+app.post('/entry/:id', (req, res) => {
+  const result1 = req.body.result1;
+  const result2 = req.body.result2;
+  const result3 = req.body.result3;
+  const result4 = req.body.result4;
+  var point_A = 0;
+  var point_B = 0;
+  if (Number(result1) > Number(result3) && Number(result2) > Number(result4)) {
+    point_A = 5;
+    point_B = 0;
+  } else if (Number(result1) < Number(result3) && Number(result2) < Number(result4)) {
+    point_A = 0;
+    point_B = 5;
+  } else if (Number(result1) > Number(result3) && Number(result2) < Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else if (Number(result1) < Number(result3) && Number(result2) > Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else {
+    console.log('エラー');
+  }
+  console.log("チェックここから");
+  console.log(point_A);
+  console.log(point_B);
+  console.log("チェックここまで");
+  connection.query(
+    'UPDATE game SET result1 = ?, result2 = ?, result3 = ?, result4 = ?, point_A = ?, point_B = ? WHERE game_id = ?',
+    [ req.body.result1, req.body.result2 ,req.body.result3, req.body.result4, point_A, point_B, req.params.id ],
+    (error, results) => {
+      res.redirect('/match');
     }
   );
 });
@@ -110,11 +151,11 @@ app.post('/create', (req, res) => {
   
 });
 
-// DBから削除するルーティング
+// DBからコートを削除するルーティング
 app.post('/delete/:id', (req,res) => {
   // 処理
   connection.query(
-    'UPDATE team SET court = NULL WHERE id = ?',
+    'UPDATE team SET court= null, number = null WHERE id = ?',
     [ req.params.id ],
     (error,results) => {
       // 一覧画面にリダイレクトする処理
