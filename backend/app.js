ea622373e77ccc450cf6dad68b24056b70562376
tetsu.hira <svc.snowdrop@gmail.com>
2021-07-16 08:14:02 +0900

    7/16の朝の状態

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 // 選択されたチーム名を受け取る
 // const team1 = document.getElementById('select1').value;
 
+
 const corsOptions = {
   origin: 'http://example.com',
   optionsSuccessStatus: 200
@@ -40,10 +41,8 @@ app.get('/', (req, res) => {
 
 app.get('/index', (req, res) => {
   connection.query(
-    'SELECT * FROM team; SELECT * FROM court',
+    'SELECT * FROM team; SELECT * FROM court; update team D, (SELECT C.id, C.name, sum(C.point_AB) AS sum_pointAB, sum(C.score_AB) AS sum_scoreAB FROM (select B.id, B.name, sum(A.point_A) as point_AB, sum(A.result1) + sum(A.result2) - sum(A.result3) - sum(A.result4) as score_AB from game A inner join team B on A.court = B.court collate utf8_general_ci and A.number_A = B.number group by B.id union all select B.id, B.name, sum(A.point_B) as point_AB, sum(A.result3) + sum(A.result4) - sum(A.result1) - sum(A.result2) as score_AB from game A inner join team B on A.court = B.court collate utf8_general_ci and A.number_B = B.number group by B.id) as C group by C.id ) E set D.pre_point = E.sum_pointAB, D.pre_score = E.sum_scoreAB where D.id = E.id',
     (error, results) => {
-      console.log(results[0]);
-      console.log(results[1]);
       res.render('index.ejs', {items: results[0], courts: results[1]});
     }
   );
@@ -52,11 +51,17 @@ app.get('/index', (req, res) => {
 // DBから表に反映させるルーティング
 app.get('/match', (req, res) => {
   connection.query(
-    'SELECT * FROM team',
+    'SELECT * FROM team; SELECT * FROM court; SELECT * FROM game',
     (error, results) => {
-      console.log("確認につかう");
-      console.log(results);
-      res.render('match.ejs', {teams: results});
+      res.render('match.ejs', {teams: results[0], courts: results[1], games: results[2]});
+    }
+  );
+});
+app.get('/midway', (req, res) => {
+  connection.query(
+    'SELECT * FROM team; SELECT * FROM court; SELECT * FROM game',
+    (error, results) => {
+      res.render('midway.ejs', {teams: results[0], courts: results[1], games: results[2]});
     }
   );
 });
@@ -67,23 +72,136 @@ app.get('/edit/:id', (req, res) => {
     'SELECT * FROM team WHERE id = ?; SELECT * FROM court',
     [ req.params.id ],
     (error, results) => {
-      console.log("配列修正の結果↓");
-      console.log(results[0]);
-      console.log(results[1]);
       res.render('edit.ejs', {teams: results[0], courts: results[1]});
+    }
+  );
+});
+app.get('/edit2/:id', (req, res) => {
+  connection.query(
+    'SELECT * FROM team WHERE id = ?; SELECT * FROM court',
+    [ req.params.id ],
+    (error, results) => {
+      res.render('edit2.ejs', {teams: results[0], courts: results[1]});
     }
   );
 });
 // DBを更新するルーティング
 app.post('/update/:id', (req, res) => {
-  console.log("ここから結果");
-  console.log(req.params.id);
-  console.log("ここまで")
   connection.query(
-    'UPDATE team SET court = ? WHERE id = ?',
-    [ req.body.itemName, req.params.id ],
+    'UPDATE team SET court = ? WHERE id = ?;UPDATE team SET number = ?  WHERE id = ?',
+    [ req.body.itemName, req.params.id, req.body.itemNum, req.params.id ],
     (error, results) => {
       res.redirect('/index');
+    }
+  );
+});
+app.post('/update2/:id', (req, res) => {
+  connection.query(
+    'UPDATE team SET mid_court = ? WHERE id = ?;UPDATE team SET mid_number = ?  WHERE id = ?',
+    [ req.body.itemName, req.params.id, req.body.itemNum, req.params.id ],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+// 点数を登録するルーティング
+app.post('/entry/:id', (req, res) => {
+  const result1 = req.body.result1;
+  const result2 = req.body.result2;
+  const result3 = req.body.result3;
+  const result4 = req.body.result4;
+  var point_A = 0;
+  var point_B = 0;
+  if (Number(result1) > Number(result3) && Number(result2) > Number(result4)) {
+    point_A = 5;
+    point_B = 0;
+  } else if (Number(result1) < Number(result3) && Number(result2) < Number(result4)) {
+    point_A = 0;
+    point_B = 5;
+  } else if (Number(result1) > Number(result3) && Number(result2) < Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else if (Number(result1) < Number(result3) && Number(result2) > Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else {
+    console.log('エラー');
+  }
+  console.log("チェックここから");
+  console.log(point_A);
+  console.log(point_B);
+  console.log("チェックここまで");
+  connection.query(
+    'UPDATE game SET result1 = ?, result2 = ?, result3 = ?, result4 = ?, point_A = ?, point_B = ? WHERE game_id = ?',
+    [ req.body.result1, req.body.result2 ,req.body.result3, req.body.result4, point_A, point_B, req.params.id ],
+    (error, results) => {
+      res.redirect('/match');
+    }
+  );
+});
+app.post('/entry2/:id', (req, res) => {
+  const result1 = req.body.result1;
+  const result2 = req.body.result2;
+  const result3 = req.body.result3;
+  const result4 = req.body.result4;
+  var point_A = 0;
+  var point_B = 0;
+  if (Number(result1) > Number(result3) && Number(result2) > Number(result4)) {
+    point_A = 5;
+    point_B = 0;
+  } else if (Number(result1) < Number(result3) && Number(result2) < Number(result4)) {
+    point_A = 0;
+    point_B = 5;
+  } else if (Number(result1) > Number(result3) && Number(result2) < Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else if (Number(result1) < Number(result3) && Number(result2) > Number(result4)) {
+    if (Number(result1) + Number(result2) > Number(result3) + Number(result4)) {
+      point_A = 3;
+      point_B = 1;
+    } else if (Number(result1) + Number(result2) < Number(result3) + Number(result4)) {
+      point_A = 1;
+      point_B = 3;
+    } else {
+      point_A = 2;
+      point_B = 2;
+    }
+  } else {
+    console.log('エラー');
+  }
+  console.log("チェックここから");
+  console.log(point_A);
+  console.log(point_B);
+  console.log("チェックここまで");
+  connection.query(
+    'UPDATE game SET result1 = ?, result2 = ?, result3 = ?, result4 = ?, point_A = ?, point_B = ? WHERE game_id = ?',
+    [ req.body.result1, req.body.result2 ,req.body.result3, req.body.result4, point_A, point_B, req.params.id ],
+    (error, results) => {
+      res.redirect('/match');
     }
   );
 });
@@ -109,12 +227,29 @@ app.post('/create', (req, res) => {
   
 });
 
-// DBから削除するルーティング
+// DBからコートを削除するルーティング
 app.post('/delete/:id', (req,res) => {
   // 処理
   connection.query(
-    'UPDATE team SET court = NULL WHERE id = ?',
+    'UPDATE team SET court= null, number = null WHERE id = ?',
     [ req.params.id ],
+    (error,results) => {
+      // 一覧画面にリダイレクトする処理
+      res.redirect('/index');
+    }
+  );
+});
+
+// 結果を反映するルーティング
+app.post('/total', (req,res) => {
+  // 処理
+  console.log("ここから");
+  console.log(point_A);
+  console.log(point_B);
+  console.log("ここまで");
+  connection.query(
+    'update team D, (SELECT C.id, C.name, sum(C.point_AB) AS sum_pointAB, sum(C.score_AB) AS sum_scoreAB FROM (select B.id, B.name, sum(A.point_A) as point_AB, sum(A.result1) + sum(A.result2) - sum(A.result3) - sum(A.result4) as score_AB from game A inner join team B on A.court = B.court collate utf8_general_ci and A.number_A = B.number group by B.id union all select B.id, B.name, sum(A.point_B) as point_AB, sum(A.result3) + sum(A.result4) - sum(A.result1) - sum(A.result2) as score_AB from game A inner join team B on A.court = B.court collate utf8_general_ci and A.number_B = B.number group by B.id) as C group by C.id ) E set D.pre_point = E.sum_pointAB, D.pre_score = E.sum_scoreAB where D.id = E.id',
+    [ req.body.Number ],
     (error,results) => {
       // 一覧画面にリダイレクトする処理
       res.redirect('/index');
